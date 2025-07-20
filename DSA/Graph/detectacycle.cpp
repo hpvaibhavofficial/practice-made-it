ee479dd6625a0929bc0fb457504657172eb21718
Vaibhav Bhardwaj <hpvaibhavofficial@gmail.com>
2025-07-20 10:57:07 +0530

    20-07-2025

--- DSA/Graph/detectacycle.cpp
+++ DSA/Graph/detectacycle.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+bool dfs(vector<vector<int>> &adj, vector<int> &vis,int node,int parent ){
+    vis[node]=1;
+    for(int x : adj[node]){
+        if(!vis[x]){
+            if(dfs(adj,vis,x,node) == true){ return true; } 
+        }
+        else if(x!=parent){
+            return true;
+        }
+    }
+    return false;
+}
+bool hasCycle(vector<vector<int>> &adj, int n,vector<int> &vis){
+    for(int i = 0;i<n;i++){
+        if(!vis[i]){
+            if(dfs(adj,vis,i,-1) == true) return true ;
+        } 
+    }
+    return false;
+}
 // bool detectCycle(vector<vector<int>> &adj, int n,vector<int> &vis,int src){
 //     queue<pair<int,int>> q;
 //     q.push({src,-1});
@@ -39,16 +60,21 @@ int main(){
     }
     vector<int> vis(n,0);
 
-    for(int i=0;i<n;i++){
-        if(!vis[i]){
-            if(detectaCycle(adj,n,vis,i)){
-                cout<<"Cycle detected."<<endl;
-                return 0;
-            }
-        }
-    }
+    // for(int i=0;i<n;i++){
+    //     if(!vis[i]){
+    //         if(detectaCycle(adj,n,vis,i)){
+    //             cout<<"Cycle detected."<<endl;
+    //             return 0;
+    //         }
+    //     }
+    // }
 
-    cout<<"No cycle detected.";
+    //for dfs
+    if(hasCycle(adj,n,vis)){
+        cout<<"Cycle detected."<<endl;
+    }else{
+        cout<<"No cycle detected.";
+    }            
     return 0;
 }
 
@@ -62,25 +88,26 @@ int main(){
 // 4 1 
 
 //detect a cycle using bfs
-bool detectaCycle(vector<vector<int>> &adj, int n,vector<int> &vis,int src){
-    queue<pair<int,int>> q;
-    q.push({src,-1});
-    vis[src] =1;
+// bool detectaCycle(vector<vector<int>> &adj, int n,vector<int> &vis,int src){
+//     queue<pair<int,int>> q;
+//     q.push({src,-1});
+//     vis[src] =1;
 
-    while(!q.empty()){
-        int node = q.front().first;
-        int parent = q.front().second;
-        q.pop();
-        for(int x : adj[node]){
-            if(!vis[x]){
-                q.push({x,node});
-                vis[x]=1;
-            }
-            else if(x!=parent){
-                return true;
-            }
-        }
-    }
-    return false;
+//     while(!q.empty()){
+//         int node = q.front().first;
+//         int parent = q.front().second;
+//         q.pop();
+//         for(int x : adj[node]){
+//             if(!vis[x]){
+//                 q.push({x,node});
+//                 vis[x]=1;
+//             }
+//             else if(x!=parent){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
 
-}
+// }
+
